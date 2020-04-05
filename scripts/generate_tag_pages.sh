6041e283d3089270cae9dc5dc7d3eb0fd1e38568
Giovanni Sciortino <giovannibattistasciortino@gmail.com>
2020-04-05 16:23:29 +0200

    Add dynamically generated tag pages based on https://longqian.me/2017/02/09/github-jekyll-tag/

---
+++ scripts/generate_tag_pages.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+EXTRACT_TAGS_FROM_POSTS=$(grep --no-filename tags _posts/*|cut -d"[" -f2|cut -d"]" -f1| tr ',' '\n'|sed 's/^ *//g'|sort|uniq)
+for TAG in $EXTRACT_TAGS_FROM_POSTS ; do
+cat << 'EOF' > tag/${TAG}.md
+---
+title:  ""
+tag: ${TAG}
+---
+{% include collecttags.html %}
+<div class="post">
+<h2>Posts with tag: {{ page.tag }}</h2>
+<ul>
+{% for post in site.tags[page.tag] %}
+  <li><a href="{{ post.url }}">{{ post.title }}</a> ({{ post.date | date_to_string }})<br>
+    {{ post.description }}
+  </li>
+{% endfor %}
+</ul>
+</div>
+<hr>
+EOF
+done
---
+++ tag/python.md
@@ -0,0 +1,16 @@
+---
+title:  ""
+tag: python
+---
+{% include collecttags.html %}
+<div class="post">
+<h2>Posts with tag: {{ page.tag }}</h2>
+<ul>
+{% for post in site.tags[page.tag] %}
+  <li><a href="{{ post.url }}">{{ post.title }}</a> ({{ post.date | date_to_string }})<br>
+    {{ post.description }}
+  </li>
+{% endfor %}
+</ul>
+</div>
+<hr>
